da586256e55c61bdf2ad5ba404fb94555c943c2a
Salohiddin <163408119+Nigmatullayev001@users.noreply.github.com>
2025-08-03 21:46:07 +0500

    Update unlimited_money.lua

--- unlimited_money.lua
+++ unlimited_money.lua
@@ -1,15 +1,2 @@
--- Bu script o'yin ichida LocalScript sifatida ishlaydi (faqat o'z ekraningda)
--- Real pul o'zgarishi bo'lmaydi, faqat GUI ko'rinishida
-
-local player = game.Players.LocalPlayer
-local leaderstats = player:WaitForChild("leaderstats")
-local moneyStat = leaderstats:FindFirstChild("Money") or leaderstats:FindFirstChild("Cash")
-
-if moneyStat then
-    while true do
-        moneyStat.Value = moneyStat.Value + 100000
-        wait(0.5)
-    end
-else
-    warn("Money yoki Cash topilmadi.")
-end
+-- Masalan topilgan Remote nomi "AddMoney"
+game:GetService("ReplicatedStorage").AddMoney:FireServer(9999999)
