2cb115c476960fe4b9266de0a4341cff8eba0018
Salohiddin <163408119+Nigmatullayev001@users.noreply.github.com>
2025-08-04 16:36:53 +0500

    Update i.lua

--- i.lua
+++ i.lua
@@ -1,6 +1,17 @@
--- Revive / Respawn kod
+-- Safe Speed toggle script
+local UIS = game:GetService("UserInputService")
 local Players = game:GetService("Players")
-local player = Players.LocalPlayer
+local LocalPlayer = Players.LocalPlayer
+local Humanoid = LocalPlayer.Character:WaitForChild("Humanoid")
 
--- Character respawn
-player:LoadCharacter()
+local NormalSpeed = 16 -- default Roblox speed
+local FastSpeed = 25 -- pastroq boost (anticheatdan o‘tishi mumkin)
+local Speeding = false
+
+UIS.InputBegan:Connect(function(input, gpe)
+	if gpe then return end
+	if input.KeyCode == Enum.KeyCode.LeftShift then
+		Speeding = not Speeding
+		Humanoid.WalkSpeed = Speeding and FastSpeed or NormalSpeed
+	end
+end)
